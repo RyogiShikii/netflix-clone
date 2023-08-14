3ef7277e4909ac6d500cdb8516ec8a9b96bc6195
Daniel <guosongraiden@gmail.com>
2023-08-13 23:01:30 -0700

    Auth
    Add Authentication UI
    next-auth, prisma, mongo setup

---
+++ pages/auth.tsx
@@ -0,0 +1,115 @@
+import Input from "@/components/input";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import {signIn} from 'next-auth/react';
+import { useRouter} from 'next/router';
+
+
+const Auth = () => {
+  const router = useRouter();
+
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [variant, setVariant] = useState("login");
+
+  const toggleVariant = useCallback(() => {
+    setVariant((currentVariant) =>
+      currentVariant === "login" ? "register" : "login"
+    );
+  }, []);
+
+  const login = useCallback(async () => {
+    try {
+      await signIn('credentials', {
+        email,
+        password,
+        redirect: false,
+        callbackUrl: '/'
+      })
+
+      router.push('/');
+    } catch (error) {
+      console.log(error)
+    }
+  },[email, password, router])
+
+  const register = useCallback(async () => {
+    try {
+      await axios.post('/api/register', {
+        email,
+        name,
+        password
+      });
+
+      login();
+    } catch (error) {
+      console.log(error)
+    }
+  },[email, name, password])
+
+  return (
+    <div className="relative h-full w-full bg-[url('/images/hero.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
+      <div className="bg-black w-full h-full lg:bg-opacity-50">
+        <nav className="px-12 py-5">
+          <img src="/images/logo.png" alt="logo" className="h-12" />
+        </nav>
+        <div className="flex justify-center">
+          <div className=" bg-black bg-opacity-70 px-16 py-16 self-center mt-2 lg:w-2/5 lg:max-w-md rounded-md w-full">
+            <h2 className=" text-white text-4xl mb-8 font-semibold">
+              {variant === "login" ? "Sign In" : "Register"}
+            </h2>
+            <div className=" flex flex-col gap-4">
+              {variant === "register" && (
+                <Input
+                  label="Username"
+                  onChange={(event: any) => {
+                    setName(event.target.value);
+                  }}
+                  id="name"
+                  value={name}
+                />
+              )}
+              <Input
+                label="Email"
+                onChange={(event: any) => {
+                  setEmail(event.target.value);
+                }}
+                id="email"
+                value={email}
+                type="email"
+              />
+
+              <Input
+                label="Password"
+                onChange={(event: any) => {
+                  setPassword(event.target.value);
+                }}
+                id="password"
+                value={password}
+                type="password"
+              />
+            </div>
+            <button onClick={variant === 'login'? login : register} className="bg-red-600 py-3 text-white rounded-md w-full mt-10 hover:bg-red-700 transition">
+              {variant === "login" ? "Login" : "Sign up"}
+            </button>
+            <p className="text-neutral-500 mt-12">
+              {variant === "login"
+                ? "first time using Netflix?"
+                : "Already have an account?"}
+              <span
+                onClick={toggleVariant}
+                className="text-white ml-1 hover:underline cursor-pointer"
+              >
+                {variant === "login" ? "Create an account" : "Sign in"}
+              </span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Auth;
